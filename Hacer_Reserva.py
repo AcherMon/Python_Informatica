93a1ad376f1c694ded975091dfac5c49f7a2b9b1
AcherMon <achermontuenga2008@gmail.com>
2025-03-07 11:49:52 +0100

    Primero Commit

---
+++ Hacer_Reserva.py
@@ -0,0 +1,23 @@
+def hacer_reserva (sitios,reserva_personal,nombre):
+    sitios=[]
+    print ("Esta es la lista de sitios ya reservados")
+    for n in range(10):
+        sitios.append({"Reserva":"False","Nombre":""})
+        asiento=sitios[1]
+        asiento["Reserva"]="True"
+        asiento["Nombre"]="John Doe"
+        for n in range(10):
+            print(sitios[n])
+            reserva_personal=int(input("¿Que sitio quiere reservar?"))
+            if reserva_personal==(1):
+                print ("Ese sitio ya esta ocupado")
+            else:
+                if reserva_personal>9:
+                    print("Ese sitio no existe")
+                else:
+                    nombre=input("¿A nombre de quien?")
+                    asiento=sitios[reserva_personal]
+                    asiento["Reserva"]="True"
+                    asiento["Nombre"]=nombre
+                    for n in range(10):
+                        print(sitios[n])
