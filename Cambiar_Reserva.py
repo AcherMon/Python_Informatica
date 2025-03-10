d6bf3b1814645c2dd56572232adf302738642051
AcherMon <achermontuenga2008@gmail.com>
2025-03-10 10:13:31 +0100

    quito commit bis

--- Cambiar_Reserva.py
+++ Cambiar_Reserva.py
@@ -1,23 +1,23 @@
-print("Esta es la lista de sitos ya reservados")
-sitios=[]
-numero=10
-for n in range(numero):
-    sitios.append({"Reserva":"False","Nombre":""})
-asiento=sitios[1]
-asiento["Reserva"]="True"
-asiento["Nombre"]="John Doe"
-for n in range(numero):
-    print(sitios[n])
-reserva_personal=int(input("¿En que sitio estaba?"))
-if reserva_personal==(1):
-    reserva_personal=int(input("¿A que sitio quiere cambiarse?"))
-    asiento=sitios[reserva_personal]
+def cambiar_Reserva (sitios,reserva_personal):
+    print("Esta es la lista de sitos ya reservados")
+    sitios=[]
+    for n in range(10):
+        sitios.append({"Reserva":"False","Nombre":""})
+    asiento=sitios[1]
     asiento["Reserva"]="True"
     asiento["Nombre"]="John Doe"
-    asiento=sitios[1]
-    asiento["Reserva"]="False"
-    asiento["Nombre"]=""
-    for n in range(numero):
+    for n in range(10):
         print(sitios[n])
-else:
-    print ("Ese sitio no esta ocupado")
+    reserva_personal=int(input("¿En que sitio estaba?"))
+    if reserva_personal==(1):
+        reserva_personal=int(input("¿A que sitio quiere cambiarse?"))
+        asiento=sitios[reserva_personal]
+        asiento["Reserva"]="True"
+        asiento["Nombre"]="John Doe"
+        asiento=sitios[1]
+        asiento["Reserva"]="False"
+        asiento["Nombre"]=""
+        for n in range(10):
+            print(sitios[n])
+    else:
+        print ("Ese sitio no esta ocupado")
