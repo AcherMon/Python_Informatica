bd16308e69b4226ec48fa85e9130508652fc2397
AcherMon <achermontuenga2008@gmail.com>
2025-03-10 10:10:07 +0100

    Quinto Commit

--- El_Gran_Programa.py
+++ El_Gran_Programa.py
@@ -1,19 +1,28 @@
-from Hacer_Reserva import *
-
-
 sitios=[{"Nombre":""},{"Nombre":""},{"Nombre":""},{"Nombre":""}]
-
 print("Hola, bienvenido a la pagina de Renfe")
 print("1-Hacer Reserva")
 print("2-Cancelar Reserva")
 print("3-Cambiar Reserva")
 accion=int(input("¿Que quiere hacer"))
 
+from Hacer_Reserva import *
+from Cancelar_Reserva import *
+from Cambiar_Reserva import *
 
+
+nombre=input("¿A nombre de quien?")
 
 if accion==(1):
-    sitios = hacer_reserva(sitios)
-    print(sitios)
-
-if accion==(2):
-    sitios = cambiar_reserva(sitios)
+    reserva_personal=int(input("¿Que sitio quiere reservar?"))
+    hacer_reserva(sitios,reserva_personal,nombre)
+    
+else:
+    if accion==(2):
+            reserva_personal=int(input("¿Que reserva quiere cancelar?"))
+            cancelar_reserva(sitios,reserva_personal,nombre)
+            
+    else:
+        if accion==(3):
+            reserva_personal=int(input("¿A que sitio quiere cambiarse?"))
+            cambiar_Reserva(sitios,reserva_personal,nombre)
+            
